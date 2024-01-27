63986db2d93ec45effaeead711d2c91f7d9422dd
Jonathan Loving <eprisencc@gmail.com>
2024-01-26 21:43:32 -0500

    Added tabbar view.

---
+++ WeOut/Views/TripsView.swift
@@ -0,0 +1,18 @@
+//
+//  TripsView.swift
+//  WeOut
+//
+//  Created by Jonathan Loving on 1/26/24.
+//
+
+import SwiftUI
+
+struct TripsView: View {
+    var body: some View {
+        Text("TripsView")
+    }
+}
+
+#Preview {
+    TripsView()
+}
