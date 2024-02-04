b7abd24b3801dd97dd95375cc6f1750f2db4ce98
Jonathan Loving <eprisencc@gmail.com>
2024-02-04 15:35:35 -0500

    Starting work on trip view.

---
+++ WeOut/Sheet/TripsInputSheet.swift
@@ -0,0 +1,18 @@
+//
+//  TripsInputSheet.swift
+//  WeOut
+//
+//  Created by Jonathan Loving on 2/4/24.
+//
+
+import SwiftUI
+
+struct TripsInputSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TripsInputSheet()
+}
--- WeOut/Views/TripsView.swift
+++ WeOut/Views/TripsView.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct TripsView: View {
     var body: some View {
-        Text("TripsView")
+        ZStack{
+            Color(hex: "#003459")
+                .ignoresSafeArea()
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Trips")
+                        .font(.largeTitle)
+                        .foregroundStyle(Color.white)
+                        .bold()
+                    Spacer()
+                    //Bring up the itinerary input sheet
+                    Button {
+                    }
+                label: {
+                    Image(systemName: "plus")
+                } 
+                .foregroundColor(.white)
+                .font(.largeTitle)
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(25)
+        }
     }
 }
 
