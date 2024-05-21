1146d661eece619991a1520b18a1ac647088bf9b
Jonathan Loving <eprisencc@gmail.com>
2024-05-21 10:27:16 -0400

    Adding firebase to app.

--- WeOut/WeOutApp.swift
+++ WeOut/WeOutApp.swift
@@ -6,22 +6,26 @@
 //
 
 import SwiftUI
+import SwiftUI
 import FirebaseCore
-import Firebase
-import FirebaseFirestore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
     return true
   }
 }
+  
 
 @main
 struct WeOutApp: App {
     // register app delegate for Firebase setup
-      @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    init() {
+        // Use Firebase library to configure APIs
+        FirebaseApp.configure()
+    }
     
     @StateObject var myTrips = Trips()
     var body: some Scene {
